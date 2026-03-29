3d97471b0c0605a8b050ff2e25432eb21ba5e02c
Konstantin_Zhe <konstantin4ik33@gmail.com>
2026-03-29 23:41:41 +0300

    Initial commit

---
+++ src/test/java/com/itk/jsonView/JsonViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itk.jsonView;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
