2d7eea75912c5735886ae9c38b91ee3452f2795b
Konstantin_Zhe <konstantin4ik33@gmail.com>
2026-03-30 05:12:18 +0300

    add JsonView annotation to Order Entity

--- src/main/java/com/itk/jsonView/model/Order.java
+++ src/main/java/com/itk/jsonView/model/Order.java
@@ -1,5 +1,9 @@
 package com.itk.jsonView.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonView;
+import com.itk.jsonView.views.Views;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -14,19 +18,27 @@ import java.util.UUID;
 @Entity
 @Table(name = "orders")
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Order {
 
     @Id
+    @JsonView(Views.UserDetails.class)
     private UUID id;
 
+    @JsonView(Views.UserDetails.class)
     private String products;
 
+    @JsonView(Views.UserDetails.class)
     private BigDecimal amount;
 
+    @JsonView(Views.UserDetails.class)
     private LocalDateTime createdAt;
+
+    @JsonView(Views.UserDetails.class)
     private LocalDateTime updatedAt;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
+    @JsonIgnore
     private User user;
 }
